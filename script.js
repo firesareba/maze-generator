4a82cf7bc0a4af22701cbbbdd1ba3206a5475a11
firesareba <101755187+firesareba@users.noreply.github.com>
2025-12-28 23:27:27 -0800

    finish origin shift and start display
    all logic for the origin shift is done, now i'm just trying to display it. Copied over the draw_line functin from enigma

--- script.js
+++ script.js
@@ -20,24 +20,75 @@ size.addEventListener("change", function(e){
 });
 
 function origin_shift(){
-    for (let i = 0; i<maze.length; i++){
-        for (let j = 0; j<maze.length; j++){//maze.length = maze[0].length
-            maze[i][j] = '→';
+    //create base
+    for (let x = 0; x<maze.length; x++){
+        for (let y = 0; y<maze.length; y++){//maze.length = maze[0].length
+            maze[x][y] = '→';
         }
-        maze[i][maze.length-1] = '↓';
+        maze[x][maze.length-1] = '↓';
     }
-    maze[maze.length-1][maze.length-1] = 'O'
+    origin_pos = [maze.length-1, maze.length-1];
+    maze[origin_pos[0]][origin_pos[1]] = 'O';
+
+
+    dir_move = {
+        '←':[-1, 0],
+        '→':[1, 0], 
+        '↑':[0, -1], 
+        '↓':[0, 1]
+    }
+
+    for (let i = 0; i<1000; i++){
+        direction_choices = [];
+        if (origin_pos[0]-1 > 0){
+            direction_choices.push('←');
+        }
+        if (origin_pos[0]+1 < maze.length){
+            direction_choices.push('→');
+        }
+        if (origin_pos[1]-1 > 0){
+            direction_choices.push('↑');
+        }
+        if (origin_pos[1]+1 < maze.length){
+            direction_choices.push('↓');
+        }
+
+        direction = direction_choices[Math.floor(Math.random()*direction_choices.length)];
+        maze[origin_pos[0]][origin_pos[1]] = direction;
+        origin_pos = [origin_pos[0]+dir_move[direction][0], origin_pos[1]+dir_move[direction][1]];
+        maze[origin_pos[0]][origin_pos[1]] = 'O';
+    }
 }
 
 function generate_maze(){
     maze = [];
-    for (let i=0; i<size.value; i++){
+    for (let x=0; x<size.value; x++){
         maze.push([]);
-        for (let j=0; j<size.value; j++){
-            maze[i].push(" ");
+        for (let y=0; y<size.value; y++){
+            maze[x].push(" ");
         }
     }
     if (generation_method.value == "origin-shift"){
         origin_shift()
+        display_maze()
     }
+}
+
+function display_maze(){
+    draw_line(0, 0, 450, 450)
+}
+
+function draw_line(x1, y1, x2, y2) {
+    const canvas_rect = canvas.getBoundingClientRect();
+    x1 = x1-canvas_rect.left;
+    y1 = y1-canvas_rect.left;
+    x2 = x2-canvas_rect.left;
+    y2 = y2-canvas_rect.left;
+
+    drawable_canvas.beginPath();
+    drawable_canvas.lineWidth = 5;
+    drawable_canvas.strokeStyle = "antiquewhite";
+    drawable_canvas.moveTo(x1, y1);
+    drawable_canvas.lineTo(x2, y2);
+    drawable_canvas.stroke();
 }
