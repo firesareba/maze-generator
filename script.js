9af7e3d6bdd9ed638cef31fea0004a33b38c35a6
firesareba <101755187+firesareba@users.noreply.github.com>
2025-12-28 22:42:28 -0800

    create base maze for origin shift
    the way origin shift works is theres one origin that gets moved, which changes the maze. But you need a basic maze to start with. The one i made is basically just ones that has all rows seperated except last column is free

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 const size = document.getElementById("size");
 const size_label = document.getElementById("size-label");
+const generation_method = document.getElementById("generation-method");
+const canvas = document.getElementById("display")
+const drawable_canvas = canvas.getContext("2d");
 
 var maze = []
+generate_maze()
+console.log(size.value);
+console.log(maze);
 
 size.addEventListener("input", function(e){
     size_label.innerHTML = "Size: "+ size.value;
 });
 
 size.addEventListener("change", function(e){
+    generate_maze()
     console.log(size.value);
+    console.log(maze);
+});
+
+function origin_shift(){
+    for (let i = 0; i<maze.length; i++){
+        for (let j = 0; j<maze.length; j++){//maze.length = maze[0].length
+            maze[i][j] = '→';
+        }
+        maze[i][maze.length-1] = '↓';
+    }
+    maze[maze.length-1][maze.length-1] = 'O'
+}
+
+function generate_maze(){
     maze = [];
     for (let i=0; i<size.value; i++){
         maze.push([]);
+        for (let j=0; j<size.value; j++){
+            maze[i].push(" ");
+        }
     }
-    console.log(maze);
-});
+    if (generation_method.value == "origin-shift"){
+        origin_shift()
+    }
+}
